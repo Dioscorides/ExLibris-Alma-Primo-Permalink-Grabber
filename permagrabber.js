629b0567c70f0324baf0ab111074128a7720c82b
Giulio Menna <Giulio.menna@gmail.com>
2017-04-28 11:25:12 +0200

    Inital release

--- permagrabber.js
+++ permagrabber.js
@@ -1,3 +1,5 @@
 $(".EXLSummary").find(".EXLSummaryFields").each(function () {
+    var almaId = $(".EXLResultSnippet", this).attr("id").replace("text you want to replace", "new text"); // this grabs the Alma ID and cleans it - edit as needed
+    var shelfmark = $(".EXLResultFourthLine", this).text().replace("text you want to replace", "new text"); // this grabs the shelfmark IF DISPLAYED - edit as needed
     console.log(shelfmark, almaId);
 });
